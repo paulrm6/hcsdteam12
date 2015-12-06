08779d0ac490b46d575fe12ed96b1368b368a0a6
ikamelot <metalrockersengkin@hotmail.com>
2015-12-06 01:13:01 +0000

    Maintenance class
    Class to manage and maintain the information in the appointment form

---
+++ com2002/assignment/calendar/gui/DisplayBoard.java
@@ -0,0 +1,24 @@
+package com2002.assignment.calendar.gui;
+
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class DisplayBoard extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private Color BACKGROUND_COLOUR = Color.white;
+   
+   /** The appointment form's background colour is set to white **/
+	public DisplayBoard() { 
+		super();
+		setBackground (BACKGROUND_COLOUR);
+	}
+   
+   /** Fix the current data **/
+	public void setPresentDay (LinkedList days) {}
+   
+   /** A Graphics object is deployed to visually show the JPanel and repaints this JPanel **/
+	public void paintComponent (Graphics g) {
+   	  super.paintComponent(g);   	  
+	}
+}
