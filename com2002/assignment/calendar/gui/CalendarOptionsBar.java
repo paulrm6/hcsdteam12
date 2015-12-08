15889a4ce7e4df4a8e4cc38e0c3db649e7d051d1
ikamelot <metalrockersengkin@hotmail.com>
2015-12-08 21:53:28 +0000

    Remove irrelevant documentations
    Replace them with short comments

--- com2002/assignment/calendar/gui/CalendarOptionsBar.java
+++ com2002/assignment/calendar/gui/CalendarOptionsBar.java
@@ -6,65 +6,50 @@ import javax.swing.*;
 
 public class CalendarOptionsBar extends JToolBar {
 	private static final long serialVersionUID = 1L;
-
-	/** A Jbutton for placing a new appointment into the calendar**/
-	public static JButton newAppointmentButton;
-	
-	/** A Jbutton for removing an appointment from the calendar **/
-	public static JButton deleteAppointmentButton;
-	
-	/** A Jbutton for viewing the previous month **/
+	public static JButton makeAppointmentButton;
+	public static JButton cancelAppointmentButton;
 	public static JButton previousMonthButton;
-	
-	/** A Jbutton for viewing the next month **/
 	public static JButton nextMonthButton;
-	
-	/** A Jbutton for causing the previous year to viewed **/
 	public static JButton previousYearButton;
-	
-	/** A Jbutton for causing the next year to viewed **/
 	public static JButton nextYearButton;
-	
-	/** stores the JButtons for iterator access **/
-	private Vector buttons;
+	private Vector toggles;
 
-	/** Initializes every member JButton and adds it to the JToolBar **/
+	// Launches every JButton objects and inserts them into the calendar' options bar 
 	public CalendarOptionsBar(ActionListener component ) {
-
 		super();
-		buttons = new Vector(6);
+		toggles = new Vector(6);
 		
-		newAppointmentButton = new JButton("New Appointment");
-		buttons.addElement(newAppointmentButton);
-		add(newAppointmentButton);
+		makeAppointmentButton = new JButton("Insert New Appointment");
+		toggles.addElement(makeAppointmentButton);
+		add(makeAppointmentButton);
 		
-		deleteAppointmentButton = new JButton("Delete Appointment");
-		add(deleteAppointmentButton);
-		buttons.addElement(deleteAppointmentButton);
+		cancelAppointmentButton = new JButton("Delete An Appointment");
+		add(cancelAppointmentButton);
+		toggles.addElement(cancelAppointmentButton);
 		
 		previousMonthButton = new JButton("Previous Month");
 		add(previousMonthButton);
-		buttons.addElement(previousMonthButton);
+		toggles.addElement(previousMonthButton);
 		
 		nextMonthButton = new JButton("Next Month");
 		add(nextMonthButton);
-		buttons.addElement(nextMonthButton);
+		toggles.addElement(nextMonthButton);
 		
 		previousYearButton = new JButton("Previous Year");
 		add(previousYearButton);
-		buttons.addElement(previousYearButton);
+		toggles.addElement(previousYearButton);
 		
 		nextYearButton = new JButton("Next Year");
 		add(nextYearButton);
-		buttons.addElement(nextYearButton);
+		toggles.addElement(nextYearButton);
 		
 		setListener(component);
 		}
-
-	/** Enables ActionListener objects to be associated as a listener with every JButton in the toolbar**/ 
+ 
+	//Every ActionListener objects is linked with every JButton in the calendar's options bar
 	public void setListener(ActionListener listener) {
-		for(int i = 0; i < buttons.size(); i++) {
-			((JButton)buttons.elementAt(i)).addActionListener(listener);
+		for(int i = 0; i < toggles.size(); i++) {
+			((JButton)toggles.elementAt(i)).addActionListener(listener);
 		}
 	}
 }
