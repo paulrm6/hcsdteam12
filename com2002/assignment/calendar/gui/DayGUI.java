a7fb72d47f03266ba53ef911e3e6d53fea9d8389
ikamelot <metalrockersengkin@hotmail.com>
2015-12-09 17:28:50 +0000

    Initial commit
    Initial source code

---
+++ com2002/assignment/calendar/gui/DayGUI.java
@@ -0,0 +1,32 @@
+package com2002.assignment.calendar.gui;
+/** Implemented to display the window that allow appointments to be added or cancelled. */
+import com2002.assignment.calendar.data.*;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class DayGUI extends JPanel {
+	private static final long serialVersionUID = 1L;
+	
+	public JLabel narrativeText; //JLabel stores date in text form
+	private Day day; //Day object
+	
+	public static Color BACKGROUND_COLOUR = Color.white;
+	public static Color HIGHLIGHT_COLOUR = Color.lightGray;
+
+	public DayGUI() {
+		super();
+		day = new Day();
+		String data = day.toString();
+		narrativeText = new JLabel(data);
+		add(narrativeText);
+		highlight(false);
+	}
+
+	public void highlight(boolean setHighlight) {
+	if (setHighlight)
+		setBackground(HIGHLIGHT_COLOUR);
+	else
+		setBackground(BACKGROUND_COLOUR);	
+	}
+}
