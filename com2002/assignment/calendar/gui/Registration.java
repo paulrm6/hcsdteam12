bc7d6fe32b904ea74fe6e7d9248f421475585fbf
Joseph Chung <jchung2@sheffield.ac.uk>
2015-12-09 23:13:26 +0000

    Changed layout manager
    Cleaner design with more flexibility to change layout

--- com2002/assignment/calendar/gui/Registration.java
+++ com2002/assignment/calendar/gui/Registration.java
@@ -3,54 +3,91 @@
  */
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class Registration extends JFrame {
 
+    private JLabel titleLabel, forenameLabel, surnameLabel, dobLabel, phoneLabel,
+                    houseLabel, streetLabel, districtLabel, cityLabel, postcodeLabel;
+    private JComboBox title;
+    private JTextField forename, surname, dob, phone,
+                        house, street, district, city, postcode;
+    private JButton submit;
+
+    public Registration() {
+
+        String[] titles = new String[] {"Mr","Mrs","Miss","Ms","Dr"};
+        setTitle("Registration form");
+        setLayout(new GridBagLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        titleLabel = new JLabel("Title:");
+        forenameLabel = new JLabel("Forename:");
+        surnameLabel = new JLabel("Surname:");
+        dobLabel = new JLabel("Date of birth:");
+        phoneLabel = new JLabel("Phone number:");
+        houseLabel = new JLabel("House number:");
+        streetLabel = new JLabel("Street:");
+        districtLabel = new JLabel("District:");
+        cityLabel = new JLabel("City:");
+        postcodeLabel = new JLabel("Postcode:");
+
+        title = new JComboBox(titles);
+        forename = new JTextField(10);
+        surname = new JTextField(10);
+        dob = new JTextField(5);
+        phone = new JTextField(10);
+        house = new JTextField(1);
+        street = new JTextField(10);
+        district = new JTextField(10);
+        city = new JTextField(10);
+        postcode = new JTextField(5);
+
+        submit = new JButton("Register");
+
+        add(titleLabel,0,0,1,1);
+        add(forenameLabel,0,1,1,1);
+        add(surnameLabel,0,2,1,1);
+        add(dobLabel,0,3,1,1);
+        add(phoneLabel,0,4,1,1);
+        add(houseLabel,0,5,1,1);
+        add(streetLabel,0,6,1,1);
+        add(districtLabel,0,7,1,1);
+        add(cityLabel,0,8,1,1);
+        add(postcodeLabel,0,9,1,1);
+
+        add(title,1,0,1,1);
+        add(forename,1,1,1,1);
+        add(surname,1,2,1,1);
+        add(dob,1,3,1,1);
+        add(phone,1,4,1,1);
+        add(house,1,5,1,1);
+        add(street,1,6,1,1);
+        add(district,1,7,1,1);
+        add(city,1,8,1,1);
+        add(postcode,1,9,1,1);
+
+        add(submit,1,10,1,1);
+        setBounds(100,100,300,350);
+        setVisible(true);
+
+    }
+
+    public void add(Component c, int x, int y, int w, int h) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = x; // horizontal
+        gbc.gridy = y; // vertical
+        gbc.gridwidth = w;
+        gbc.gridheight = h;
+        gbc.ipadx = 20; // padding between the components horizontally
+        gbc.ipady = 5; // padding between the components vertically
+        gbc.anchor = GridBagConstraints.WEST;
+        add(c, gbc);
+    }
+
     public static void main(String[] args) {
 
-        String [] titles = new String[] {"Mr", "Ms", "Miss", "Mrs", "Dr"};
-        JComboBox title = new JComboBox(titles);
-        JTextField forename = new JTextField();
-        JTextField surname = new JTextField();
-        JTextField dob = new JTextField();
-        JTextField phone = new JTextField();
-        JTextField house = new JTextField();
-        JTextField street = new JTextField();
-        JTextField district = new JTextField();
-        JTextField city = new JTextField();
-        JTextField postcode = new JTextField();
-        JButton submit = new JButton("Submit");
-
-        JFrame box = new JFrame("Registration form");
-        box.setSize(500,500);
-        GridLayout grid = new GridLayout(11,2,30,0);
-        box.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        box.setLayout(grid);
-        box.add(new JLabel("Title: "));
-        box.add(title);
-        box.add(new JLabel("Forename: "));
-        box.add(forename);
-        box.add(new JLabel("Surname: "));
-        box.add(surname);
-        box.add(new JLabel("Date of birth: "));
-        box.add(dob);
-        box.add(new JLabel("Phone number: "));
-        box.add(phone);
-        box.add(new JLabel("House number: "));
-        box.add(house);
-        box.add(new JLabel("Street: "));
-        box.add(street);
-        box.add(new JLabel("District: "));
-        box.add(district);
-        box.add(new JLabel("City: "));
-        box.add(city);
-        box.add(new JLabel("Postcode: "));
-        box.add(postcode);
-        box.add(new JLabel(""));
-        box.add(submit);
-
-        box.setVisible(true);
+        new Registration();
 
     }
 
