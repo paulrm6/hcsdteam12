0957c64d68955909bbddd728cd1f05fa5c322098
Joseph Chung <jchung2@sheffield.ac.uk>
2015-12-09 23:17:47 +0000

    Added comments

--- com2002/assignment/calendar/gui/Registration.java
+++ com2002/assignment/calendar/gui/Registration.java
@@ -8,6 +8,7 @@ import java.awt.*;
 
 public class Registration extends JFrame {
 
+    // Creation of all variables needed for the form
     private JLabel titleLabel, forenameLabel, surnameLabel, dobLabel, phoneLabel,
                     houseLabel, streetLabel, districtLabel, cityLabel, postcodeLabel;
     private JComboBox title;
@@ -17,10 +18,17 @@ public class Registration extends JFrame {
 
     public Registration() {
 
+        // Array of titles to add into combo box
         String[] titles = new String[] {"Mr","Mrs","Miss","Ms","Dr"};
+
+        //set properties of the frame
         setTitle("Registration form");
         setLayout(new GridBagLayout());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100,100,300,350);
+        setVisible(true);
+
+        // Creation of labels
         titleLabel = new JLabel("Title:");
         forenameLabel = new JLabel("Forename:");
         surnameLabel = new JLabel("Surname:");
@@ -32,6 +40,7 @@ public class Registration extends JFrame {
         cityLabel = new JLabel("City:");
         postcodeLabel = new JLabel("Postcode:");
 
+        // Creation of components
         title = new JComboBox(titles);
         forename = new JTextField(10);
         surname = new JTextField(10);
@@ -42,9 +51,9 @@ public class Registration extends JFrame {
         district = new JTextField(10);
         city = new JTextField(10);
         postcode = new JTextField(5);
+        submit = new JButton("Register"); // button
 
-        submit = new JButton("Register");
-
+        // Laying out the labels with GridBagConstraint
         add(titleLabel,0,0,1,1);
         add(forenameLabel,0,1,1,1);
         add(surnameLabel,0,2,1,1);
@@ -56,6 +65,7 @@ public class Registration extends JFrame {
         add(cityLabel,0,8,1,1);
         add(postcodeLabel,0,9,1,1);
 
+        // Laying out the components with GridBagConstraint
         add(title,1,0,1,1);
         add(forename,1,1,1,1);
         add(surname,1,2,1,1);
@@ -66,13 +76,11 @@ public class Registration extends JFrame {
         add(district,1,7,1,1);
         add(city,1,8,1,1);
         add(postcode,1,9,1,1);
-
-        add(submit,1,10,1,1);
-        setBounds(100,100,300,350);
-        setVisible(true);
+        add(submit,1,10,1,1); // button
 
     }
 
+    // function for adding to the layout
     public void add(Component c, int x, int y, int w, int h) {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = x; // horizontal
@@ -87,6 +95,7 @@ public class Registration extends JFrame {
 
     public static void main(String[] args) {
 
+        // Creating the GUI
         new Registration();
 
     }
