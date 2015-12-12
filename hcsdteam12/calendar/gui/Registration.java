890d2d839aba3608751d237e533d6eae13abdfe6
Joseph Chung <jchung2@sheffield.ac.uk>
2015-12-12 15:32:59 +0000

    Added validation
    Date of birth mask will not work after clearing.. not sure how to
    fix..lots of repeated code..

--- hcsdteam12/calendar/gui/Registration.java
+++ hcsdteam12/calendar/gui/Registration.java
@@ -1,22 +1,32 @@
-package hcsdteam12.calendar.gui;
-
 /**
  * Created by Joseph on 08/12/2015.
  */
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
+import javax.swing.text.MaskFormatter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Registration extends JFrame {
 
     // Creation of all variables needed for the form
     private JLabel titleLabel, forenameLabel, surnameLabel, dobLabel, phoneLabel,
-                    houseLabel, streetLabel, districtLabel, cityLabel, postcodeLabel;
+                    houseLabel, streetLabel, districtLabel, cityLabel, postcodeLabel,
+                    forenameError, surnameError, dobError, phoneError,
+                    houseError, streetError, districtError, cityError, postcodeError;
     private JComboBox title;
-    private JTextField forename, surname, dob, phone,
+    private JTextField forename, surname, phone,
                         house, street, district, city, postcode;
+    private JFormattedTextField dob;
     private JButton submit;
+
+    private final DateFormat date = new SimpleDateFormat("dd/mm/yyyy");
 
     public Registration() {
 
@@ -32,19 +42,27 @@ public class Registration extends JFrame {
         titleLabel = new JLabel("Title:");
         forenameLabel = new JLabel("Forename:");
         surnameLabel = new JLabel("Surname:");
-        dobLabel = new JLabel("Date of birth:");
+        dobLabel = new JLabel("Date of birth (dd/mm/yyyy):");
         phoneLabel = new JLabel("Phone number:");
         houseLabel = new JLabel("House number:");
         streetLabel = new JLabel("Street:");
         districtLabel = new JLabel("District:");
         cityLabel = new JLabel("City:");
         postcodeLabel = new JLabel("Postcode:");
+        forenameError = new JLabel("");
+        surnameError = new JLabel("");
+        dobError = new JLabel("");
+        phoneError = new JLabel("");
+        houseError = new JLabel("");
+        streetError = new JLabel("");
+        districtError = new JLabel("");
+        cityError = new JLabel("");
+        postcodeError = new JLabel("");
 
         // Creation of components
         title = new JComboBox(titles);
         forename = new JTextField(10);
         surname = new JTextField(10);
-        dob = new JTextField(5);
         phone = new JTextField(10);
         house = new JTextField(1);
         street = new JTextField(10);
@@ -52,6 +70,15 @@ public class Registration extends JFrame {
         city = new JTextField(10);
         postcode = new JTextField(5);
         submit = new JButton("Register"); // button
+        // Setting the date of birth function to take a particular format
+        dob = new JFormattedTextField(date);
+        dob.setColumns(5);
+        try {
+            MaskFormatter dateMask = new MaskFormatter("##/##/####");
+            dateMask.install(dob);
+        } catch (ParseException ex){
+            Logger.getLogger(Registration.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
         // Laying out the labels with GridBagConstraint
         add(titleLabel,0,0,1,1);
@@ -64,6 +91,15 @@ public class Registration extends JFrame {
         add(districtLabel,0,7,1,1);
         add(cityLabel,0,8,1,1);
         add(postcodeLabel,0,9,1,1);
+        add(forenameError,2,1,1,1);
+        add(surnameError,2,2,1,1);
+        add(dobError,2,3,1,1);
+        add(phoneError,2,4,1,1);
+        add(houseError,2,5,1,1);
+        add(streetError,2,6,1,1);
+        add(districtError,2,7,1,1);
+        add(cityError,2,8,1,1);
+        add(postcodeError,2,9,1,1);
 
         // Laying out the components with GridBagConstraint
         add(title,1,0,1,1);
@@ -78,8 +114,78 @@ public class Registration extends JFrame {
         add(postcode,1,9,1,1);
         add(submit,1,10,1,1); // button
 
+        // Validation
+        submit.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                if (forename.getText().toString().matches("[a-zA-z]+")) {
+                    forenameError.setForeground(Color.GREEN);
+                    forenameError.setText("✓");
+                } else {
+                    forenameError.setForeground(Color.RED);
+                    forenameError.setText("X");
+                }
+                if (surname.getText().toString().matches("[a-zA-z]+")) {
+                    surnameError.setForeground(Color.GREEN);
+                    surnameError.setText("✓");
+                } else {
+                    surnameError.setForeground(Color.RED);
+                    surnameError.setText("X");
+                }
+                if (dob.getText().toString().matches("([0-9]{4})/([0-9]{2})/([0-9]{2})")) {
+                    dobError.setForeground(Color.GREEN);
+                    dobError.setText("✓");
+                } else {
+                    dobError.setForeground(Color.RED);
+                    dobError.setText("X");
+                }
+                if (phone.getText().toString().matches("[0-9]+") && phone.getText().length() == 11) {
+                    phoneError.setForeground(Color.GREEN);
+                    phoneError.setText("✓");
+                } else {
+                    phoneError.setForeground(Color.RED);
+                    phoneError.setText("X");
+                }
+                if (house.getText().toString().matches("[0-9a-zA-z]+")) {
+                    houseError.setForeground(Color.GREEN);
+                    houseError.setText("✓");
+                } else {
+                    houseError.setForeground(Color.RED);
+                    houseError.setText("X");
+                }
+                if (street.getText().toString().matches("[a-zA-Z]+")) {
+                    streetError.setForeground(Color.GREEN);
+                    streetError.setText("✓");
+                } else {
+                    streetError.setForeground(Color.RED);
+                    streetError.setText("X");
+                }
+                if (district.getText().toString().matches("[a-zA-Z]+")) {
+                    districtError.setForeground(Color.GREEN);
+                    districtError.setText("✓");
+                } else {
+                    districtError.setForeground(Color.RED);
+                    districtError.setText("X");
+                }
+                if (city.getText().toString().matches("[a-zA-Z]+")) {
+                    cityError.setForeground(Color.GREEN);
+                    cityError.setText("✓");
+                } else {
+                    cityError.setForeground(Color.RED);
+                    cityError.setText("X");
+                }
+                if (postcode.getText().toString().matches("[a-zA-Z0-9]+") &&
+                        postcode.getText().length() >= 5 && postcode.getText().length() <= 7) {
+                    postcodeError.setForeground(Color.GREEN);
+                    postcodeError.setText("✓");
+                } else {
+                    postcodeError.setForeground(Color.RED);
+                    postcodeError.setText("X");
+                }
+            }
+        });
+
         // Setting size and visibility
-        setBounds(100,100,300,350);
+        setBounds(100,100,400,350);
         setVisible(true);
 
     }
@@ -91,9 +197,9 @@ public class Registration extends JFrame {
         gbc.gridy = y; // vertical
         gbc.gridwidth = w;
         gbc.gridheight = h;
+        gbc.anchor = GridBagConstraints.WEST;
         gbc.ipadx = 20; // padding between the components horizontally
         gbc.ipady = 5; // padding between the components vertically
-        gbc.anchor = GridBagConstraints.WEST;
         add(c, gbc);
     }
 
