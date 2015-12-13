c9da93a167196ee5ac2aa554afe1f5c18ea97a35
Paul MacDonald <paulrm6@gmail.com>
2015-12-13 12:06:49 +0000

    Update the program overview

--- hcsdteam12/DentalPractice.java
+++ hcsdteam12/DentalPractice.java
@@ -10,21 +10,25 @@ public class DentalPractice {
 
     public static void main (String args[]) {
         /**
-         * Start by displaying a main screen with the different options that are needed
-         * Options:
-         * Secretary -
+         * Start by displaying a main screen with the different users that are needed
+         * Users:
+         * Secretary
+         * Partner
+         *
+         * Once selected display the following options for Secretary
          * Register Patient
          * Update patient info (contained within is health care plan stuff)
-         * Register appointment
-         * Find and/or cancel appointment (display on calendar)
+         * Book appointment
+         * Find and/or cancel appointment (display on calendar WEEKLY VIEW)
          * Record when paid
          * Book holiday (empty appointment)
-         * Review unpaid treatments given to a patient as a list listing costs and total cost (auto uses hleathcare plan)
+         * Review unpaid treatments given to a patient as a list listing costs and total cost (auto uses healthcare plan)
+         *
+         *
          * Partner -
          * View appointments for week
          * Record having seen patient, put in treatments given and cost
          */
-        System.out.println("Test");
         MainCalendar.display();
 
     }
